4a275c6dfc59a8293168da16d05e28317cfa406f
gedev <geniilsonfernandes@gmail.com>
2024-11-13 16:25:06 -0300

    fix: remove menu

--- src/modules/site/components/Menu/Menu.tsx
+++ src/modules/site/components/Menu/Menu.tsx
@@ -1,22 +1,14 @@
 import { Link } from 'react-router-dom';
-import { Button, Flex } from '@mantine/core';
+import { Flex } from '@mantine/core';
 import { Logo } from '@/shared/components/Logo/Logo';
 import routes from '@/shared/constant/routes';
 
 export const Menu = () => {
   return (
-    <Flex py="lg" align="center" justify="space-between">
+    <Flex py="lg" align="center" justify="center">
       <Link to={routes.home}>
         <Logo />
       </Link>
-      <Flex align="center" gap="sm">
-        <Button variant="transparent" size="sm">
-          Contato
-        </Button>
-        <Button variant="light" size="sm">
-          Como funciona
-        </Button>
-      </Flex>
     </Flex>
   );
 };
